101a4d3b794cc6db6ed5f845be91a096a76035c8
Marcodaum <marco.daum@schanzer-racing.de>
2024-02-01 09:37:44 +0100

    updated bubblesort and mergesort

--- sortingAlgorithms.hs
+++ sortingAlgorithms.hs
@@ -2,12 +2,13 @@
 mergesort :: Ord a => [a] -> [a]
 mergesort [] = []
 mergesort [a] = [a]
-mergesort x = merge (mergesort (take ((length x) `div` 2) x)) (mergesort (drop ((length x) `div` 2) x))
+mergesort x = merge (mergesort (take halveLength x)) (mergesort (drop halveLength x))
     where
+        halveLength = (length x) `div` 2
         merge :: Ord a => [a] -> [a] -> [a]
         merge [] x = x
         merge x [] = x
-        merge (x:xs) (y:ys) | y > x = x:(merge xs (y:ys))
+        merge (x:xs) (y:ys) | y > x     = x:(merge xs (y:ys))
                             | otherwise = y:(merge (x:xs) ys)
 
 
@@ -27,11 +28,15 @@ insertionsort (x:xs) = inssort x (insertionsort xs)
     where
         inssort :: Ord a => a -> [a] -> [a]
         inssort a [] = [a]
-        inssort x (y:ys) | x <= y = x:y:ys
+        inssort x (y:ys) | x <= y    = x:y:ys
                          | otherwise = y : (inssort x ys)
 
 
 -- Binarysort
+binarysort :: [Int] -> [Int]
+binarysort [] = []
+binarysort xs = flatten (list2tree xs)
+
 data Tree = Val Int | Node Tree Tree Tree | Nil
     deriving Show
 
@@ -41,24 +46,24 @@ list2tree (x:xs) = (insertElem (list2tree xs) (Val x))
 
 insertElem :: Tree -> Tree -> Tree
 insertElem Nil val = val
-insertElem (Val x) (Val y) | x <= y = Node Nil (Val x) (Val y)
-                           | otherwise = Node (Val y) (Val x) Nil
-insertElem (Node (x) (Val y) (z)) (Val a) | a <= y = Node (insertElem x (Val a)) (Val y) (z)
-                                          | a > y = Node (x) (Val y) (insertElem z (Val a))
+insertElem (Val x) (Val y) | x <= y     = Node Nil (Val x) (Val y)
+                           | otherwise  = Node (Val y) (Val x) Nil
+insertElem (Node (x) (Val y) (z)) (Val a) | a <= y    = Node (insertElem x (Val a)) (Val y) (z)
+                                          | otherwise = Node (x) (Val y) (insertElem z (Val a))
 
 flatten :: Tree -> [Int]
 flatten Nil = []
 flatten (Val x) = [x]
 flatten (Node (x) (Val y) (z)) = (flatten x) ++ [y] ++ (flatten z)
 
-binarysort :: [Int] -> [Int]
-binarysort [] = []
-binarysort xs = flatten (list2tree xs)
-
 
 -- Bubblesort
-bubblesorty :: Ord a => [a] -> [a]
-bubblesorty [] = []
-bubblesorty [x] = [x]
-bubblesorty (x:y:xs) | x < y = bubblesort (x:(bubblesort (y:xs)))
-                     | x >= y = bubblesort (y:(bubblesort (x:xs)))
+bubblesort :: Ord a => [a] -> [a]
+bubblesort [] = []
+bubblesort [x] = [x]
+bubblesort (x:y:xs) | isSorted (x:y:xs) = (x:y:xs)
+                    | x < y             = bubblesort (x:(bubblesort (y:xs)))
+                    | otherwise         = bubblesort (y:(bubblesort (x:xs)))
+
+isSorted :: Ord a => [a] -> Bool
+isSorted x = and [a <= b | (a, b) <- zip x (tail x)]
